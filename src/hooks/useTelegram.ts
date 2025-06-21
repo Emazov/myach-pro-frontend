f039e3f35fafe1e7e6ac2cd200cf2b6e410c2dbf
Emazov <emir.almazovich@gmail.com>
2025-06-21 15:41:03 +0600

    init

--- src/hooks/useTelegram.ts
+++ src/hooks/useTelegram.ts
@@ -3,7 +3,6 @@ const tg = window.Telegram.WebApp;
 export function useTelegram() {
 	return {
 		tg,
-		initData:
-			'query_id=AAHKvtMXAwAAAMq-0xfmjNcU&user=%7B%22id%22%3A6842203850%2C%22first_name%22%3A%22Emir%22%2C%22last_name%22%3A%22%22%2C%22username%22%3A%22emazovdev%22%2C%22language_code%22%3A%22ru%22%2C%22allows_write_to_pm%22%3Atrue%2C%22photo_url%22%3A%22https%3A%5C%2F%5C%2Ft.me%5C%2Fi%5C%2Fuserpic%5C%2F320%5C%2F33-xPnEyJ-IyE3GNJ4sqXoDzSWkK0GVwRzKCzU_XGcRDYa_XCQ-7QxvEezWyEQ-x.svg%22%7D&auth_date=1750482722&signature=r_oOhZv2gDzQnZmQZG1v7dPPCCYNWMiwBAM3m__T8Tj393WQRiDyYPTlJdCCuzsqWBeBoiN3YERonImhOgzcBw&hash=80d9f40479185c7570bdf681c5bd47be785a03b57f1aa38dd6db92ccf4198046',
+		initData: tg.initData,
 	};
 }
