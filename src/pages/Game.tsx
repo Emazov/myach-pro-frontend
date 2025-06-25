45f22289dd235a4c208808447c065727b1a491b7
Emazov <emir.almazovich@gmail.com>
2025-06-25 18:29:00 +0600

    ui

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -196,13 +196,23 @@ const Game = () => {
 
 			<div className='hero flex flex-col items-center gap-4 relative'>
 				{canGoBack && (
-					<button
+					<svg
 						onClick={handleGoBack}
-						className='absolute left-0 top-1/2 -translate-y-1/2 w-12 h-12 bg-[#EC3381] rounded-full flex items-center justify-center text-white hover:bg-opacity-70 transition-all z-10'
-						aria-label='Вернуться к предыдущему игроку'
+						width='30'
+						height='30'
+						viewBox='0 0 40 40'
+						fill='none'
+						xmlns='http://www.w3.org/2000/svg'
 					>
-						<span>←</span>
-					</button>
+						<circle cx='20' cy='20' r='20' fill='#EC3381' />
+						<path
+							d='M22.75 27.5L15.25 20L22.75 12.5'
+							stroke='white'
+							strokeWidth='2'
+							strokeLinecap='round'
+							strokeLinejoin='round'
+						/>
+					</svg>
 				)}
 
 				{player?.img_url && (
