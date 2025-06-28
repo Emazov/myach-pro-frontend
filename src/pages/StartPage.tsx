d8fbd98e33c1ba801cb42eb7015f18b01363c392
Emazov <emir.almazovich@gmail.com>
2025-06-28 17:03:29 +0600

    stable: fix: улучшена логика отображения статуса пользователя на главной странице

--- src/pages/StartPage.tsx
+++ src/pages/StartPage.tsx
@@ -23,8 +23,6 @@ const StartPage = () => {
 		preloadCriticalImages();
 	}, []);
 
-	console.log('update')
-
 	return (
 		<div className='welcome bg-[url("/main_bg.jpg")] bg-cover bg-center h-full'>
 			<div className='container flex flex-col justify-around h-full'>
@@ -36,17 +34,13 @@ const StartPage = () => {
 					<img src='./main_logo.png' alt='logo' className='logo' />
 
 					{/* Отображаем статус пользователя */}
-					{isLoading ? (
-						<div className='mt-4 bg-white/80 p-3 rounded-lg text-center'>
-							<p className='font-medium'>Проверка пользователя...</p>
-						</div>
-					) : telegramId && isAdmin ? (
+					{telegramId && isAdmin && (
 						<div className='mt-4 bg-white/80 p-3 rounded-lg text-center'>
 							<div className='flex flex-col gap-1'>
 								<p className='font-bold text-green-600'>Администратор</p>
 							</div>
 						</div>
-					) : null}
+					)}
 				</div>
 
 				{/* Разные кнопки для админов и обычных пользователей */}
@@ -56,7 +50,7 @@ const StartPage = () => {
 						to={isAdmin ? '/admin' : '/guide'}
 						className='link_btn bg-[#FFEC13] text-[clamp(1rem,2vh,1.5rem)] text-black py-[clamp(1rem,1vh,2rem)]'
 					>
-						{isAdmin ? 'Админ' : 'Поехали!'}
+						{isLoading ? 'Подождите...' : isAdmin ? 'Админ' : 'Поехали!'}
 					</Link>
 				</div>
 			</div>
